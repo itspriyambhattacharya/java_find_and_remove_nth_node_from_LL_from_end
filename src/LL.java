5fbe4aeeaebb9c6dfa8b3efbfafaca116993c9f8
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-11-10 11:11:28 +0530

    added addLast() method to add elements at last

--- src/LL.java
+++ src/LL.java
@@ -6,4 +6,18 @@ public class LL {
         this.head = null;
         this.size = 0;
     }
+
+    public void addLast(int data){
+        Node n = new Node(data);
+        this.size++;
+        if (head == null){
+            head = n;
+            return;
+        }
+        Node ptr = head;
+        while(ptr.link != null){
+            ptr = ptr.link;
+        }
+        ptr.link = n;
+    }
 }
