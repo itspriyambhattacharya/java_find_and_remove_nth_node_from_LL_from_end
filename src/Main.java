aa0d30d1f7955aa85bfb054fc280e784041cbaa1
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-11-10 11:16:18 +0530

    added dummy code to run the program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         LL list = new LL();
+
+        list.addLast(45);
+        list.addLast(5);
+        list.addLast(55);
+        list.addLast(67);
+        list.addLast(12);
+        list.addLast(64);
+        list.addLast(89);
+        list.display();
     }
 }
