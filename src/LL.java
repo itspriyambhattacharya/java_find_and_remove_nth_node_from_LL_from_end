92e66c24c7dfd122e30ee717edcaaf90a851ae84
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-11-10 12:01:13 +0530

    added the find and delete logic

--- src/LL.java
+++ src/LL.java
@@ -2,35 +2,47 @@ public class LL {
     public Node head;
     public int size;
 
-    public LL(){
+    public LL() {
         this.head = null;
         this.size = 0;
     }
 
-    public void addLast(int data){
+    public void addLast(int data) {
         Node n = new Node(data);
         this.size++;
-        if (head == null){
+        if (head == null) {
             head = n;
             return;
         }
         Node ptr = head;
-        while(ptr.link != null){
+        while (ptr.link != null) {
             ptr = ptr.link;
         }
         ptr.link = n;
     }
 
-    public void display(){
+    public void display() {
         Node ptr = head;
-        while(ptr !=  null){
+        while (ptr != null) {
             System.out.print(ptr.data + "----->");
             ptr = ptr.link;
         }
         System.out.println("null");
     }
 
-    public int getSize(){
+    public int getSize() {
         return this.size;
+    }
+
+    public int delete(int pos) {
+        Node prevNode = null;
+        Node currentNode = head;
+        for (int i = 0; i < pos; i++) {
+            prevNode = currentNode;
+            currentNode = currentNode.link;
+        }
+        int rem = currentNode.data;
+        prevNode.link = currentNode.link;
+        return rem;
     }
 }
